067aceec5c6240cd71bbc56d0974e7faf48aa3cd
srpromax <andersonbdavila00@gmail.com>
2024-04-16 21:28:56 -0500

    remove jwtparse from pezumartapi

--- src/api/pezumartApi.ts
+++ src/api/pezumartApi.ts
@@ -1,17 +1,5 @@
 import axios from "axios";
 
-let jwtParse = null;
-
-const jwt = localStorage.getItem("jwt") || ""
-
-if(jwt.trim() !== "") {
-    try {
-        jwtParse = JSON.parse(jwt)
-    } catch (error) {
-        localStorage.removeItem("jwt")
-    }
-}
-
 export const pezumartApi = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL
 })
